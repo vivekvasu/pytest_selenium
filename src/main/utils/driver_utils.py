a87a14dad311e86db3ab5d2e5f71ee868b043cbc
vivekvasu <vivekvivi39@gmail.com>
2023-12-17 12:21:24 +0530

    Update driver_utils.py

--- src/main/utils/driver_utils.py
+++ src/main/utils/driver_utils.py
@@ -7,3 +7,9 @@ class DriverUtils:
 
     def open_url(self, url: str):
         self.driver.get(url)
+
+    def enter_text(self, element, text):
+        element.send_keys(str(text))
+
+    def quit_driver(self):
+        self.driver.quit()
