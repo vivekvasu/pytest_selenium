6a7b8a0963cc4f9105ea1a0a7a8c53b81a40986b
vivekvasu <vivekvivi39@gmail.com>
2023-12-17 12:26:37 +0530

    Update config.py

--- src/main/configs/config.py
+++ src/main/configs/config.py
@@ -5,9 +5,11 @@ from typing import Any
 @dataclass
 class Config:
     url: str
+    db: str
 
     @staticmethod
     def from_dict(obj: Any) -> 'Config':
         _url = str(obj.get('url'))
-        return Config(_url)
+        _db = str(obj.get('db'))
+        return Config(_url, _db)
       
